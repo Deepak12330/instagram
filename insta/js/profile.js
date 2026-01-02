7a270d4933a9dc718bb2a1354673d66158d4010e
Deepak12330 <96013003+Deepak12330@users.noreply.github.com>
2026-01-02 20:04:37 +0530

    Update profile.js

--- insta/js/profile.js
+++ insta/js/profile.js
@@ -6,6 +6,54 @@ window.onload = function()
 	}
 	else
 	{
-		
+
+		// fetch username -----------------------------------------
+		var user_email = sessionStorage.getItem("users");
+		var text_data = localStorage.getItem(user_email);
+		var object_data = JSON.parse(text_data);
+		var username = atob(object_data.users);
+	
+		var profile_name = document.getElementById("profile_name");
+		profile_name.innerHTML = username;
+
+		//--------------------------------------------------
+
+		//start profile pic
+
+		var profile_pic = document.getElementById("profile_pic");
+		profile_pic.onchange = function()
+		{
+			var files =  profile_pic.files[0];
+			var reader = new FileReader();
+
+			reader.onload = function()
+			{
+				var profile = document.getElementById("profile");
+				var profile_icon = document.getElementById("profile_icon");
+
+				var url_image = reader.result;
+				profile.style.backgroundImage = "url("+url_image+")";
+				profile.style.backgroundSize = "cover";
+				profile_icon.style.display = "none";
+				document.getElementById("next_btn").style.display = "block";
+
+				document.getElementById("next_btn").onclick = function()
+				{
+					// var user_email = object_data.email;
+					// localStorage.setItem(user_email,url_image);
+					// console.log("oaky"); not working properly
+				}
+
+
+
+			}
+
+				reader.readAsDataURL(files);
+		}	
+
+
+
+
+
 	}
 }
