ef8007bf2ed7abdf8d07815280c156c450bda498
Deepak12330 <96013003+Deepak12330@users.noreply.github.com>
2026-01-11 11:32:52 +0530

    Add files via upload

---
+++ insta/contacts/js/contact.js
@@ -0,0 +1,9 @@
+document.getElementById("new_contact").onclick = function()
+{
+	document.getElementById("contact_bg").style.display = "block";
+}
+
+document.getElementById("close").onclick = function()
+{
+	document.getElementById("contact_bg").style.display = "none";
+}
