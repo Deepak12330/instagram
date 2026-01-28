7c431551fdf3f21fa92bb912612154290d3b894e
Deepak12330 <96013003+Deepak12330@users.noreply.github.com>
2026-01-28 19:51:33 +0530

    Update contact.js

--- insta/contacts/js/contact.js
+++ insta/contacts/js/contact.js
@@ -46,69 +46,52 @@ window.onload = function()
 	}
 
 
-
-//fetch the data from anywhere ----------------------------------
-	function allContact()
-	{
+	function allContact() {
 		var i;
-		for(i=0; i<localStorage.length; i++)
+		for(i=0;i<localStorage.length;i++)
 		{
-			var all_key =  localStorage.key(i);
-
-			if(all_key.match(sessionStorage.getItem("users")+"_contacts"))
+			var all_keys = localStorage.key(i);
+			if(all_keys.match(sessionStorage.getItem("users")+"_contacts"))
 			{
-				var text_data = localStorage.getItem(all_key);
-				var object_data = JSON.parse(text_data);
+				var json_txt = localStorage.getItem(all_keys);
+				var obj_data = JSON.parse(json_txt);
 
 
+				var contact_box = document.createElement("DIV");
+				contact_box.setAttribute("id","contact");
+				var name_p = document.createElement("P");
+				name_p.setAttribute("class","contact_name");
+				var name_i = document.createElement("I");
+				name_i.setAttribute("class","fas fa-user");
+				name_p.appendChild(name_i);
+				name_p.innerHTML += obj_data.contact_name;
 
-				// main contact div
-				var all_contact = document.createElement("div");
-				all_contact.setAttribute("id", "all_contact");
 
-				// contact box
-				var contact_box = document.createElement("div");
-				contact_box.setAttribute("id", "contact_box");
+				var tool = document.createElement("DIV");
+				tool.setAttribute("id","tool");
+				var edit_i = document.createElement("I");
+				edit_i.setAttribute("class","fas fa-edit");
+				var trash = document.createElement("I");
+				trash.setAttribute("class","fas fa-trash");
+				tool.appendChild(edit_i);
+				tool.appendChild(trash);
 
-				// icons box
-				var icons = document.createElement("div");
-				icons.setAttribute("id", "icons");
+				var line = document.createElement("HR");
+				line.setAttribute("width","75%");
+				var num_p = document.createElement("P");
 
-				// name
-				var name_p = document.createElement("p");
-				name_p.innerHTML = object_data.contact_name;
+				var num_i = document.createElement("I");
+				num_i.setAttribute("class","fas fa-mobile-alt");
+				num_p.appendChild(num_i);
+				num_p.innerHTML += obj_data.contact_number;
 
-				// number
-				var number_p = document.createElement("p");
-				number_p.innerHTML = object_data.contact_number;
 
-				// edit icon
-				var edit_i = document.createElement("i");
-				edit_i.setAttribute("class", "fa-solid fa-pen-to-square");
-
-				// delete icon
-				var trash_i = document.createElement("i");
-				trash_i.setAttribute("class", "fa-solid fa-trash");
-
-				// append icons
-				icons.appendChild(edit_i);
-				icons.appendChild(trash_i);
-
-				// append contact box
 				contact_box.appendChild(name_p);
-				contact_box.appendChild(number_p);
+				contact_box.appendChild(tool);
+				contact_box.appendChild(line);
+				contact_box.appendChild(num_p);
 
-				// append everything
-				all_contact.appendChild(contact_box);
-				all_contact.appendChild(icons);
-
-				// finally add to body or any container
-				document.getElementById("address").appendChild(all_contact)
-
-
-
-
-
+				document.getElementById("all_contact_box").appendChild(contact_box);
 
 
 
@@ -116,9 +99,26 @@ window.onload = function()
 		}
 	}
 
-
-
 	allContact();
 
-	//---------------------end -------------
+
+		var search =  document.getElementById("search");
+		search.oninput = function()
+		{
+			var all_contact_name = document.getElementsByClassName("contact_name");
+			var i;
+			for(i=0;i<all_contact_name.length;i++)
+			{
+				if(all_contact_name[i].innerHTML.match(search.value))
+				{
+					all_contact_name[i].parentElement.style.display = "block";	
+				}
+				else
+				{
+						all_contact_name[i].parentElement.style.display = "none";
+				}
+			}
+		}
+
+	
 }
